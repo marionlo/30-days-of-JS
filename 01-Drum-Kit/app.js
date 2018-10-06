1387fda28ad6df96c8929b6e1cca00e4cde3eb6f
Marion <marion.lotito@gmail.com>
2018-10-06 20:03:00 +0200

    feat: change style when playing the key

--- 01-Drum-Kit/app.js
+++ 01-Drum-Kit/app.js
@@ -1,6 +1,18 @@
- window.addEventListener('keydown', function (e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (!audio) return // stop the function if no audio
     audio.currentTime = 0; // rewind the sound to start
     audio.play();
-});
+    key.classList.add('playing');
+}
+
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playSound );
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
