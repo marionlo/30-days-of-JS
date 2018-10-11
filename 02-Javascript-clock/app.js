25249024af53551527c513c3a3afe5963f0c221d
Marion <marion.lotito@gmail.com>
2018-10-11 23:19:51 +0200

    add minutes and hours function

--- 02-Javascript-clock/app.js
+++ 02-Javascript-clock/app.js
@@ -1,4 +1,6 @@
 const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate () {
   const now = new Date();
@@ -6,6 +8,14 @@ function setDate () {
   const secondsDegrees = ((seconds / 60) * 360) + 90; //add 90 to offset the 90degrees in the CSS
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   console.log(seconds);
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60 * 360) + 90)
+  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12 * 360) + 90) // divide by 12 because it's hours
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
